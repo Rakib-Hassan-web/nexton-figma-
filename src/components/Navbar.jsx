b9507c22e0b2aadea0f32beff50eb89829f2376d
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 00:58:54 +0600

    icon add done need position

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ import logo from '../assets/image/logo.png'
 
 
 import { RiUserLine } from "react-icons/ri";
-import { CiShoppingCart } from "react-icons/ci";
+import { RiShoppingCartLine } from "react-icons/ri";
+
 
 
 
@@ -24,16 +25,16 @@ return (
 
       <div className="navsearch w-[400px] h-[52px]  bg-[#F8F8F8] rounded-[100px] flex items-center gap-[10px] pl-[24px] " id='navsearch'>
 
-     <  RiSearch2Line className='text-2xl text-praymary ' />
+     <  RiSearch2Line className='text-2xl text-praymary  srach_icon' id='srach_icon' />
         <input className='navinp w-full border-none outline-none text-[14px] font-normal text-praymary font-praymary ' placeholder='Search in products... ' id='navinp' type="text" />
 
       </div>
 
 
-      <div className="nav_icons">
+      <div className="nav_icons " id='nav_icons'>
 
-     <button><RiUserLine/></button>
-     <button><CiShoppingCart/></button>
+     <button className='user_icon text-2xl text-praymary  mr-[22px]' id='user_icon'><RiUserLine/></button>
+     <button  className='cart_icon text-2xl text-praymary ' id='cart_icon'  ><RiShoppingCartLine/></button>
 
       </div>
 
