4545f19b105e18cd9290ff62b7f56895cdaadb05
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-05 00:07:51 +0600

    logo added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import { Link } from 'react-router'
+
+import logo from '../assets/image/logo.png'
 
 const Navbar = () => {
   return (
     <>
     
-    <div className='container' id='navbar'>tumi k bhi?
-      
+    <div className='container py-[27px] ' id='navbar'>
+
+
+      <div className="nav_row">
+
+     <Link className="nav_LOGO"><img src={logo} alt="" /></Link>
+
+
+<div className="navInput"></div>
+
+
+<div className="nav_icons"></div>
+
+      </div>
+
+
+
     </div>
      
     
