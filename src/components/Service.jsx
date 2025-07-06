37e22233011affb22f1bd3ef53e4d03dd6e4e4e8
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-07 00:22:51 +0600

    service secction return icon added

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Singleservice from './comon/Singleservice'
 import { TbTruckDelivery } from 'react-icons/tb'
+import { RiExchange2Line } from 'react-icons/ri'
 
 const Service = () => {
   return (
@@ -11,7 +12,7 @@ const Service = () => {
         <div className="serviceRow container border mt-[52px] py-6 border-[#E5E7EB] rounded-[16px] flex justify-around items-center">
             <Singleservice  servicebigtxt={'Free shipping'} serviceicon={  <TbTruckDelivery className='text-3xl' />} servicesmlltxt={'On orders over $50.00'}/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
-            <Singleservice/>
+             <Singleservice  servicebigtxt={''} serviceicon={  <RiExchange2Line className='text-3xl' />} servicesmlltxt={''}/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
             <Singleservice/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
@@ -21,7 +22,8 @@ const Service = () => {
     </div>
    </section>
    
-   
+
+
    
    </>
   )
