8aec6f27a38f86858f8fedff6fabee07e114ddf3
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-07 00:18:28 +0600

    service secction 1st element

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Singleservice from './comon/Singleservice'
+import { TbTruckDelivery } from 'react-icons/tb'
 
 const Service = () => {
   return (
@@ -8,14 +9,14 @@ const Service = () => {
    <section id='service'  >
     <div className=''>
         <div className="serviceRow container border mt-[52px] py-6 border-[#E5E7EB] rounded-[16px] flex justify-around items-center">
-            <Singleservice/>
+            <Singleservice  servicebigtxt={'Free shipping'} serviceicon={  <TbTruckDelivery className='text-3xl' />} servicesmlltxt={'On orders over $50.00'}/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
             <Singleservice/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
             <Singleservice/>
             <span className=' w-[1px]  h-[50px] bg-[#E5E7EB] inline-block'></span>
             <Singleservice/>
-             hi
+            
         </div>
     </div>
    </section>
--- src/components/comon/Singleservice.jsx
+++ src/components/comon/Singleservice.jsx
@@ -3,18 +3,21 @@ import React from 'react'
 import { TbTruckDelivery } from "react-icons/tb";
 
 
-const Singleservice = () => {
+const Singleservice = ( {serviceicon ,servicebigtxt,servicesmlltxt}) => {
   return (
     <>
     
     
     
     <div className="serviceBow flex  items-center  gap-[16px] ">
-       
-   <TbTruckDelivery className='text-3xl'/>
+       {serviceicon}
+ 
    <div className="boxtxt">
-    <h2 className='text-[18px] font-medium font-praymary text-second'>Free shipping</h2>
-    <h2 className='text-praymary font-praymary  text-[14px] font-normal'>On orders over $50.00</h2>
+    <h2 className='text-[18px] font-medium font-praymary text-second'>
+        {servicebigtxt}</h2>
+    <h2 className='text-praymary font-praymary  text-[14px] font-normal'>
+        {servicesmlltxt}
+    </h2>
    </div>
 
     </div>
