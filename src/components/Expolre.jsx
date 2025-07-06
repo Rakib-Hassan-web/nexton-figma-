62c0507170fe9bf022ac862e2dd5e1b15261592e
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-07 01:27:47 +0600

    explore part header txt added

--- src/components/Expolre.jsx
+++ src/components/Expolre.jsx
@@ -6,10 +6,12 @@ const Expolre = () => {
     
 
 
-<section id='Explore '>
+<section id='Explore ' className='mt-[88px]'>
 
 <div className="container">
-    
+    <h2 className='font-praymary text-[36px] font-semibold text-second'> Start exploring.    
+        <span className='font-praymary text-[36px] font-semibold text-praymary'> Good things are waiting for you</span>
+    </h2>
 </div>
 
 
