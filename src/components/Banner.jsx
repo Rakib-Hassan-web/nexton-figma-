e95ae8c3fa76db7b2ac2d498f67fd1ebd1002501
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-06 17:50:27 +0600

    banner 1st text added with responcive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,6 +9,10 @@ const Banner = () => {
     
     <section id='banner ' style={{background: `url(${bannerimg})` ,backgroundRepeat:'no-repeat' ,backgroundSize:'cover' , backgroundPosition:'left'}} className='w-full  lg:pt-[148px] lg:pb-[168px] pt-[163px] pb-[199px] mt-[10px] lg:mt-0 '>
 
+<div className='container pl-[24px] lg:pl-0'>
+<h1 className='text-praymary text-[14px] lg:text-[20px] font-praymary font-medium'>Starting from: $49.99</h1>
+
+        </div>
     </section>
     </>
   )
