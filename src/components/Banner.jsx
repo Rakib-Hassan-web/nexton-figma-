9e09a055f5935da33941f6da2ea04fb7c1e6abbd
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-06 19:15:57 +0600

    sliing bg elements

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,25 +1,105 @@
 import React from 'react'
 import bannerimg from '../assets/image/bannerimg.png'
+import bannerextra from '../assets/image/bennerextra.png'
 import { Link } from 'react-router'
 import { CiSearch } from "react-icons/ci";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
 const Banner = () => {
-  return (
-    <>
-    
-    <section id='banner ' style={{background: `url(${bannerimg})` ,backgroundRepeat:'no-repeat' ,backgroundSize:'cover' , backgroundPosition:'left'}} className='w-full  lg:pt-[148px] lg:pb-[168px] pt-[163px] pb-[199px] mt-[10px] lg:mt-0 '>
+var settings = {
+dots: true,
+infinite: true,
+speed: 500,
+slidesToShow: 1,
+slidesToScroll: 1,
+};
+return (
+<>
+        <section id='banner '
+            style={{backgroundImage:`url(${bannerimg})` ,backgroundRepeat:'no-repeat' ,backgroundSize:'cover' , backgroundPosition:'left'}}
+            className='w-full  lg:pt-[148px] lg:pb-[168px] pt-[163px] pb-[199px] mt-[10px] lg:mt-0 '>
+    <Slider {...settings}>
 
-<div className='container pl-[24px] lg:pl-0'>
-<h1 className='text-praymary text-[14px] lg:text-[20px] font-praymary font-medium'>Starting from: $49.99</h1>
-<h2 className='text-second lg:text-[64px] text-[30px] font-semibold font-praymary lg:w-[632px] w-[312px] leading-[38px] lg:leading-[72px] lg:my-[24px]  mt-[8px] mb-[30px]'>Exclusive collection
-for everyone</h2>
+
+            <div className='container pl-[24px] lg:pl-0'>
+                <h1 className='text-praymary text-[14px] lg:text-[20px] font-praymary font-medium'>Starting from: $49.99
+                </h1>
+                <h2
+                    className='text-second lg:text-[64px] text-[30px] font-semibold font-praymary lg:w-[632px] w-[312px] leading-[38px] lg:leading-[72px] lg:my-[24px]  mt-[8px] mb-[30px]'>
+                    Exclusive collection
+                    for everyone</h2>
 
 
-<Link   to={'#'} className='bg-second flex lg:gap-3 gap-2 rounded-full items-center text-white text-[14px] lg:text-[16px] font-medium font-praymary py-[12px] lg:py-[20px] lg:pl-[36px] pl-[24px] w-fit   lg:pr-[50px] pr-[19px] hover:scale-[1.1] duration-[.4s] '> Explore now  <CiSearch className='lg:text-3xl text-2xl  '/> </Link>
-        </div>
-    </section>
-    </>
-  )
+                <Link to={'#'}
+                    className='bg-second flex lg:gap-3 gap-2 rounded-full items-center text-white text-[14px] lg:text-[16px] font-medium font-praymary py-[12px] lg:py-[20px] lg:pl-[36px] pl-[24px] w-fit   lg:pr-[50px] pr-[19px] hover:scale-[1.1] duration-[.4s] '>
+                Explore now
+                <CiSearch className='lg:text-3xl text-2xl  ' />
+                </Link>
+            </div>
+
+
+
+
+
+            <div className='container pl-[24px] lg:pl-0'>
+                <h1 className='text-praymary text-[14px] lg:text-[20px] font-praymary font-medium'>Starting from: $49.99
+                </h1>
+                <h2
+                    className='text-second lg:text-[64px] text-[30px] font-semibold font-praymary lg:w-[632px] w-[312px] leading-[38px] lg:leading-[72px] lg:my-[24px]  mt-[8px] mb-[30px]'>
+                    Exclusive collection
+                    for everyone</h2>
+
+
+                <Link to={'#'}
+                    className='bg-second flex lg:gap-3 gap-2 rounded-full items-center text-white text-[14px] lg:text-[16px] font-medium font-praymary py-[12px] lg:py-[20px] lg:pl-[36px] pl-[24px] w-fit   lg:pr-[50px] pr-[19px] hover:scale-[1.1] duration-[.4s] '>
+                Explore now
+                <CiSearch className='lg:text-3xl text-2xl  ' />
+                </Link>
+            </div>
+
+        </Slider>
+        </section>
+
+
+
+{/* 
+        <section id='banner '
+            className='w-full  lg:pt-[148px] lg:pb-[168px] pt-[163px] pb-[199px] mt-[10px] lg:mt-0  '
+
+          
+            style={{background: `url(${bannerextra})` ,backgroundRepeat:'no-repeat' ,backgroundSize:'cover' , backgroundPosition:'left'}}
+            
+            
+            
+            >
+
+
+            <div className='container pl-[24px] lg:pl-0'>
+                <h1 className='text-praymary text-[14px] lg:text-[20px] font-praymary font-medium'>Starting from: $49.99
+                </h1>
+                <h2
+                    className='text-second lg:text-[64px] text-[30px] font-semibold font-praymary lg:w-[632px] w-[312px] leading-[38px] lg:leading-[72px] lg:my-[24px]  mt-[8px] mb-[30px]'>
+                    Exclusive collection
+                    for everyone</h2>
+
+
+                <Link to={'#'}
+                    className='bg-second flex lg:gap-3 gap-2 rounded-full items-center text-white text-[14px] lg:text-[16px] font-medium font-praymary py-[12px] lg:py-[20px] lg:pl-[36px] pl-[24px] w-fit   lg:pr-[50px] pr-[19px] hover:scale-[1.1] duration-[.4s] '>
+                Explore now
+                <CiSearch className='lg:text-3xl text-2xl  ' />
+                </Link>
+            </div>
+        </section> */}
+
+
+
+
+</>
+)
 }
 
 export default Banner
