19346bab9bbe7dc21d8b98f0848bf69027ee019a
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-07 01:47:54 +0600

    explor 1st part almopst done

--- src/components/Expolre.jsx
+++ src/components/Expolre.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SingleExplore from './comon/SingleExplore'
 
 const Expolre = () => {
   return (
@@ -9,9 +10,15 @@ const Expolre = () => {
 <section id='Explore ' className='mt-[88px]'>
 
 <div className="container">
-    <h2 className='font-praymary text-[36px] font-semibold text-second'> Start exploring.    
-        <span className='font-praymary text-[36px] font-semibold text-praymary'> Good things are waiting for you</span>
+    <h2 className='font-praymary text-[36px] font-semibold text-second mb-[40px]'> Start exploring.    
+        <span className='font-praymary text-[36px] font-semibold text-praymary '> Good things are waiting for you</span>
     </h2>
+
+    <div className='border  w-[419px] p-[40px] rounded-[16px] border-[#E5E7EB]'> 
+            
+   
+    <SingleExplore exploreBigtext={"For Men's"} exploreSmalltxt={'Starting at $24'}  />
+         </div>
 </div>
 
 
--- src/components/comon/SingleExplore.jsx
+++ src/components/comon/SingleExplore.jsx
@@ -1,22 +1,35 @@
 import React from 'react'
+import { FaArrowRightLong } from 'react-icons/fa6'
+import { Link } from 'react-router'
 
-const SingleExplore = () => {
-  return (
-    <>
-    
-    
-        <div> 
-            
-        </div>
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
+const SingleExplore = ( {exploreBigtext,exploreSmalltxt,explorebutton}) => {
+return (
+<>
+
+
+    <div className="exploreBigtextt text-second font-semibold font-praymary text-[24px]">
+        {exploreBigtext}
+    </div>
+
+    <div className="exploreSmalltxtt   text-praymary font-normal font-praymary text-[14px] ">
+
+    {exploreSmalltxt}
+    </div>
+     <span className='  lg:w-[3px]  lg:h-[36px] lg:bg-[#E5E7EB] lg:inline-block'></span>
+    <div className="explorebuttonn">
+
+        
+        <Link className=''>Shop Now <FaArrowRightLong />
+        </Link>
+        {explorebutton}
+    </div>
+
+
+
+
+
+</>
+)
 }
 
 export default SingleExplore
