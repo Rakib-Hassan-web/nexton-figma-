906fb5804aa0228ce46f06ae551feb2e236fef0c
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-09 22:38:36 +0600

    explore section  proper responcive start

--- src/components/ResExpolre.jsx
+++ src/components/ResExpolre.jsx
@@ -11,7 +11,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 
 const ResExpolre = () => {
-    const settings = {
+const settings = {
 dots: true,
 infinite: true,
 speed: 500,
@@ -19,34 +19,35 @@ slidesToShow: 1,
 slidesToScroll: 1
 };
 
-  return (
-    <>
-    
-    
-     <section id='Explore ' className='mt-[88px] block lg:hidden '>
+return (
+<>
+
+
+    <section id='Explore ' className='mt-[88px] block lg:hidden '>
 
         <div className="container ">
-            <h2 className='font-praymary lg:text-[36px] text-[24px] font-semibold text-second mb-[40px]'> Start exploring.
-                <span className='font-praymary text-[36px] font-semibold text-praymary hidden lg:block '> Good things are waiting for
+            <h2 className='font-praymary lg:text-[36px] text-[24px] font-semibold text-second mb-[40px]'> Start
+                exploring.
+                <span className='font-praymary text-[36px] font-semibold text-praymary hidden lg:block '> Good things
+                    are waiting for
                     you</span>
             </h2>
 
             <Slider {...settings}>
-                <div className="EXPLORE  w-full flex  flex-wrap !gap-40 items-center ">
-                    <div className='!'>
-                        <div className='border  w-[312px] lg:w-[419px] p-[40px] inline-block rounded-[16px] border-[#E5E7EB]'>
+               
+         
 
+                <div className='border  w-[419px] py-[40px] px-6 rounded-[16px] lg:ml-[-80px] border-[#E5E7EB]'>
 
-                            <SingleExplore exploreBigtext={"For Men's"} exploreSmalltxt={'Starting at $24'}
-                                explorebutton={ <Link className='font-praymary font-medium text-[12px] text-praymary'>
-                                Shop Now
-                                <FaArrowRightLong className='text-lg' />
-                                </Link>} />
-                        </div>
-                    </div>
+                    <SingleExplore exploreBigtext={"For Men's"} exploreSmalltxt={'Starting at $24'} explorebutton={
+                        <Link
+                        className='font-praymary font-medium text-[14px] text-praymary flex items-center pl-3 gap-[8px]'>
+                        Shop
+                        Now
+
+                        <FaArrowRightLong className='text-xl' />
+                        </Link>} />
                 </div>
-
-
 
 
 
@@ -54,19 +55,23 @@ slidesToScroll: 1
                 <div className='border  w-[419px] py-[40px] px-6 rounded-[16px] lg:ml-[-80px] border-[#E5E7EB]'>
 
                     <SingleExplore exploreBigtext={"For Women's"} exploreSmalltxt={'Starting at $19'} explorebutton={
-                        <Link className='font-praymary font-medium text-[14px] text-praymary flex items-center pl-3 gap-[8px]'>Shop
+                        <Link
+                        className='font-praymary font-medium text-[14px] text-praymary flex items-center pl-3 gap-[8px]'>
+                        Shop
                         Now
-                        
+
                         <FaArrowRightLong className='text-xl' />
                         </Link>} />
                 </div>
+
+
+
 
 
                 <div className='border  w-[419px] p-[40px] rounded-[16px] lg:ml-[-40px] border-[#E5E7EB]'>
 
 
                     <SingleExplore exploreBigtext={"Accessories"} exploreSmalltxt={'Explore accessories'}
-
                         explorebutton={ <Link className='font-praymary font-medium text-[14px] text-praymary'>Shop
                         Now
                         <FaArrowRightLong className='text-xl' />
@@ -92,10 +97,10 @@ slidesToScroll: 1
     </section>
 
 
-    
-    
-    </>
-  )
+
+
+</>
+)
 }
 
 export default ResExpolre
