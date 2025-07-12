c4c1a227ca21c2cfa19792332895ece604b20930
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-13 00:11:49 +0600

    text rec

--- src/components/comon/Singleres.jsx
+++ src/components/comon/Singleres.jsx
@@ -12,9 +12,28 @@ const Singleres = () => {
  <div className="container">
     
 
- <div  className='w-[309px] h-[448px] bg-red-300 rounded-[16px]'>
+ <div  className='w-[320px] h-[448px] bg-red-300 rounded-[16px]'>
 
     <div className="recoimg w-full h-[360px] bg-amber-300 rounded-[16px]"></div>
+
+
+    <div className="recoAllTxt">
+
+
+<div className="reco1sttxtrow ">
+    <h2>Black Automatic Watch</h2>
+    <h2>$169.99</h2>
+</div>
+
+
+<div className="reco2ndtxtrow ">
+    <p>Accessories</p>
+    <p>$199.99</p>
+</div>
+
+
+
+    </div>
 
 
 
