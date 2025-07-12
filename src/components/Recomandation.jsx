a6296cb70024c17beb58769feabc105ffae6a2dd
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-12 23:26:24 +0600

    text added

--- src/components/Recomandation.jsx
+++ src/components/Recomandation.jsx
@@ -7,9 +7,8 @@ const Recomandation = () => {
     <section id='Recomandation' className='mt-[176px]'>
 
    <div className="container">
-       <h2 className=' w-[881px]font-praymary lg:text-[36px] text-[24px] font-semibold text-second mb-[40px]'> Recommendations. 
-<span className='font-praymary lg:text-[36px] font-semibold text-praymary  '> Good things are waiting for
-                    you</span>
+       <h2 className=' w-[881px]font-praymary lg:text-[36px] text-[24px] font-semibold text-second mb-[40px]'> Recommendations.  
+<span className='font-praymary lg:text-[36px] font-semibold text-praymary  '> Best matching products for you</span>
             </h2>
    </div>
     </section>
