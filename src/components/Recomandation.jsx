741d5c0c3ebe4116af4124db34d78f64c5aa86f7
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-12 23:22:39 +0600

    section er kla sru

---
+++ src/components/Recomandation.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Recomandation = () => {
+  return (
+    <>
+    
+    <section id='Recomandation' className='mt-[176px]'></section>
+    
+    
+    
+    
+    
+    
+    </>
+  )
+}
+
+export default Recomandation
--- src/components/Resomandation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Resomandation = () => {
-  return (
-    <div>Resomandation</div>
-  )
-}
-
-export default Resomandation
