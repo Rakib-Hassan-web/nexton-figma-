7eed96e84472aafbb3ee6188680b390f6b2826fa
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-18 22:50:18 +0600

    Add to cart  heading text added

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -9,10 +9,20 @@ const AddToCart = () => {
 
 <div id='cartbg'></div>
 
-<div className='w-full h-screen bg-[#000000b5]  absolute top-0 right-0'></div>
+<div className='w-full h-screen bg-[#000000b5]  absolute top-0 left-0'>
+
+<div className='w-[500px] h-screen bg-white absolute top-0 right-0'>
 
 
-<div className='w-[500px] h-screen bg-white absolute top-0 right-0'></div>
+<h2 className='text-[25px] font-semibold font-praymary text-second p-4'>AddToCart</h2>
+
+</div>
+
+
+
+</div>
+
+
 
 
 
