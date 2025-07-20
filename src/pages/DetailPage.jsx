0692374bb713e1f58d075440a689530487fbbe59
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-21 02:33:48 +0600

    detailspage add

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,33 +1,64 @@
 import React from 'react'
 
-const DetailPage = () => {
-  return (
+const DetailPage = () => (
     <>
-    
-    <section id='detail_page'>
 
-        <div className='       border-[#E5E7EB]     mt-[4px] mb-[40px]  border-t-1'></div>
-<div className="container">
-{/* ------------left side menu---------- */}
+        <section id='detail_page'>
+
+            <div className='       border-[#E5E7EB]     mt-[4px] mb-[40px]  border-t-1'></div>
+            <div className="container">
+                {/* ------------left side menu---------- */}
+<div className="LeftSide w-[285px] h-screen">
+
+                <div id='catagories' className=" mt-10">
+                    <h1 className='font-praymary font-semibold text-second text-[18px] mb-6'>Categories</h1>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Men’s fashion</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Women’s fashion</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Kids & Toys</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Accessories</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Cosmetics</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] mb-4'>Shoes</h2>
+                    <h2 className='font-praymary font-normal text-praymary text-[14px] '>Sports</h2>
+
+                </div>
+
+            <div className='       border-[#E5E7EB]     mb-[32px] mt-[40px]  border-t-1'></div>
+
+                <div className="Prizerange ">
+
+                    <h1 className='font-praymary font-semibold text-second text-[18px] mb-6'>Price range</h1>
+
+                     <div className='line'>--------------------</div>
+
+                   <div className="Prices">
+
+                 <div className="minPrice ">
+                    <h2>Min price</h2>
+                    <div className="borderMINprice w-[130px] border py-2 flex ju">
+                        <p>100</p>
+                        $
+                    </div>
+                 </div>
 
 
-<div className="catagories">
-    <h1>Categories</h1>
-    <h2>Men’s fashion</h2>
-    <h2>Women’s fashion</h2>
-    <h2>Kids & Toys</h2>
-    <h2>Accessories</h2>
-    <h2>Cosmetics</h2>
-    <h2>Shoes</h2>
-    <h2>Sports</h2>
-    <h2></h2>
-    <h2></h2>
+                   </div>
+
+                </div>
+
+
+
+
+
+
+
 </div>
 
 
 
 
 
+            </div>
+        </section>
 
 
 
@@ -36,22 +67,11 @@ const DetailPage = () => {
 
 
 
-</div>
-    </section>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
     </>
-  )
-}
+)
 
 export default DetailPage
