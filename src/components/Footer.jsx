cea81558667e9e4b4969287481485081b239e17e
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-20 15:15:28 +0600

    futter part fb youtube icon added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,83 @@
 import React from 'react'
 
+import footerlogo from '../assets/image/logo.png'
+import { FaFacebook } from 'react-icons/fa6'
+import { Link } from 'react-router'
+import { TbBrandYoutubeFilled } from 'react-icons/tb'
+
 const Footer = () => {
-  return (
-    <>
-    
-    
-    
-    
-    
+return (
+<>
+
+
+
+
+
     <section>
 
-<div className='       border-[#E5E7EB]     mt-[54px] mb-[60px]  border-t-1'></div>
+        <div className='       border-[#E5E7EB]     mt-[54px] mb-[60px]  border-t-1'></div>
+
+
+        <div className="container">
+
+            <div className="allItems">
+
+                <div className="first_col">
+                    <img src={footerlogo} alt="FooterLOGO" className='w-[119px]' />
+                    {/* -----------------Facebook--------------------- */}
+                    <div className=' mt-5 mb-3'>
+                        <Link to={'#'} className='  flex items-center gap-2'>
+                        <FaFacebook className='text-[#4676ED] text-3xl ' />
+                        <h2>Facebook</h2>
+                        </Link>
+                    </div>
+                    {/* --------------Youtube------------------ */}
+                    <div className=' mt-5 mb-3'>
+                        <Link to={'#'} className='  flex items-center gap-2'>
+                        <TbBrandYoutubeFilled className='text-[#DA0000] text-3xl  ' />
+                        <h2>Youtube</h2>
+                        </Link>
+                    </div>
+                    {/* --------------Telegram------------------ */}
+                    <div className=' mt-5 mb-3'>
+                        <Link to={'#'} className='  flex items-center gap-2'>
+                        <FaFacebook className='text-[#4676ED] text-3xl ' />
+                        <h2>Telegram</h2>
+                        </Link>
+                    </div>
+                    {/* --------------Twitter------------------ */}
+                    <div className=' mt-5 '>
+                        <Link to={'#'} className='  flex items-center gap-2'>
+                        <FaFacebook className='text-[#4676ED] text-3xl ' />
+                        <h2>Facebook</h2>
+                        </Link>
+                    </div>
+
+
+
+                </div>
 
 
 
 
 
 
+
+
+
+
+
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+        <div className='       border-[#E5E7EB]     mt-[54px] mb-[60px]  border-t-1'></div>
 
 
 
@@ -26,16 +87,16 @@ const Footer = () => {
 
 
     </section>
-    
-    
-    
-    
-    
-    
-    
-    
-    </>
-  )
+
+
+
+
+
+
+
+
+</>
+)
 }
 
 export default Footer
