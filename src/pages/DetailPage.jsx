77e7d29c3b0aba59d4e282a4b1baedc85992a4b2
Rakib-Hassan-web <rakibhassan.web@gmail.com>
2025-07-21 02:41:28 +0600

    range part low price add without line scroll

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 const DetailPage = () => (
-    <>
+<>
 
-        <section id='detail_page'>
+    <section id='detail_page'>
 
-            <div className='       border-[#E5E7EB]     mt-[4px] mb-[40px]  border-t-1'></div>
-            <div className="container">
-                {/* ------------left side menu---------- */}
-<div className="LeftSide w-[285px] h-screen">
+        <div className='       border-[#E5E7EB]     mt-[4px] mb-[40px]  border-t-1'></div>
+        <div className="container">
+            {/* ------------left side menu---------- */}
+            <div className="LeftSide w-[285px] h-screen">
 
                 <div id='catagories' className=" mt-10">
                     <h1 className='font-praymary font-semibold text-second text-[18px] mb-6'>Categories</h1>
@@ -22,26 +22,39 @@ const DetailPage = () => (
 
                 </div>
 
-            <div className='       border-[#E5E7EB]     mb-[32px] mt-[40px]  border-t-1'></div>
+                <div className='       border-[#E5E7EB]     mb-[32px] mt-[40px]  border-t-1'></div>
 
                 <div className="Prizerange ">
 
                     <h1 className='font-praymary font-semibold text-second text-[18px] mb-6'>Price range</h1>
 
-                     <div className='line'>--------------------</div>
+                    <div className='line'>--------------------</div>
 
-                   <div className="Prices">
+                    <div className="Prices flex items-center justify-between">
 
-                 <div className="minPrice ">
-                    <h2>Min price</h2>
-                    <div className="borderMINprice w-[130px] border py-2 flex justify-around items-center  rounded-full  border-[#E5E7EB]">
-                        <p className='font-praymary font-medium text-praymary text-[14px]'>100</p>
-                      <p className='font-praymary font-medium text-praymary text-[14px]'>$</p>  
+                        <div className="minPrice ">
+                            <h2 className='font-praymary font-normal text-praymary text-[16px] mb-1 mt-8'>Min price</h2>
+                            <div
+                                className="borderMINprice w-[130px] border py-2 flex justify-around items-center  rounded-full  border-[#E5E7EB]">
+                                <p className='font-praymary font-medium text-praymary text-[14px]'>100</p>
+                                <p className='font-praymary font-medium text-praymary text-[14px]'>$</p>
+                            </div>
+                        </div>
+
+
+                        <div className="MaxPrice ">
+                            <h2 className='font-praymary font-normal text-praymary text-[16px] mb-1 mt-8'>Max price</h2>
+                            <div
+                                className="borderMINprice w-[130px] border py-2 flex justify-around items-center  rounded-full  border-[#E5E7EB]">
+                                <p className='font-praymary font-medium text-praymary text-[14px]'>500</p>
+                                <p className='font-praymary font-medium text-praymary text-[14px]'>$</p>
+                            </div>
+                        </div>
+
+
+
+
                     </div>
-                 </div>
-
-
-                   </div>
 
                 </div>
 
@@ -51,14 +64,14 @@ const DetailPage = () => (
 
 
 
-</div>
-
-
-
-
-
             </div>
-        </section>
+
+
+
+
+
+        </div>
+    </section>
 
 
 
@@ -71,7 +84,7 @@ const DetailPage = () => (
 
 
 
-    </>
+</>
 )
 
 export default DetailPage
